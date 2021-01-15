77f26731e69d25bbe46294bd616c7e15ac0bebf4
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2021-01-15 01:42:23 -0300

    Implementação de estruturação da solution em areas

--- src/LFRP.UI.AppModelo/Startup.cs
+++ src/LFRP.UI.AppModelo/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,15 @@ namespace LFRP.UI.AppModelo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaPageViewLocationFormats.Clear();
+
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -32,6 +42,9 @@ namespace LFRP.UI.AppModelo
             app.UseMvc(routes=>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapAreaRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{ id ?}");
+                routes.MapAreaRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{ id ?}");
             });
         }
     }
